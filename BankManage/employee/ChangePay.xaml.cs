486cfff2f179e0bba524bd949630d28aa2dd7043
QYboy <liuguoqing0394@qq.com>
2020-12-16 12:19:50 +0800

    ChangePAY datagrid

--- BankManage/employee/ChangePay.xaml.cs
+++ BankManage/employee/ChangePay.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows.Controls;
 using System.Linq;
+using System.Windows;
+using System.Collections.Generic;
+
 namespace BankManage.employee
 {
     /// <summary>
@@ -8,17 +11,17 @@ namespace BankManage.employee
     public partial class ChangePay : Page
     {
         BankEntities context = new BankEntities();
+        List<DisplaydEmployerSalary> ListOfEmployer= new List<DisplaydEmployerSalary>();
         public ChangePay()
         {
             InitializeComponent();
             var q = from t in context.EmployeeInfo
-                    select new
-                    {
-                        职工号 = t.EmployeeNo,
-                        姓名= t.EmployeeName,
-                        工资=t.salary
-                    };
-            employee_DataGrid.ItemsSource = q.ToList();
+                    select t;
+            foreach(var i in q)
+            {
+                ListOfEmployer.Add(new DisplaydEmployerSalary(i));
+            }
+            employee_DataGrid.ItemsSource = ListOfEmployer;
         }
 
         private void saveButton_Click(object sender, System.Windows.RoutedEventArgs e)
@@ -30,6 +33,26 @@ namespace BankManage.employee
         {
 
         }
+
+        private void employee_DataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            MessageBox.Show(((DisplaydEmployerSalary)employee_DataGrid.SelectedItem).姓名);
+        }
+    }
+
+    public class DisplaydEmployerSalary
+    {
+        public string 职工号 { get; set; }
+        public string 姓名 { get; set; }
+        public decimal 工资 { get; set; }
+
+        public DisplaydEmployerSalary(EmployeeInfo t)
+        {
+            职工号 = t.EmployeeNo;
+            姓名 = t.EmployeeName;
+            工资 = t.salary.GetValueOrDefault();
+        }
+
     }
 
 }
